9a167fe37b8764cd55d37d42c041c9723dd4b9b3
Oli Roldan <oroldan@udesa.edu.ar>
2024-11-12 22:18:03 -0300

    config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdc28yFnfSLbO-jh9xlm1HQ4rOPbXQEJQ",
+  authDomain: "ti-prog3.firebaseapp.com",
+  projectId: "ti-prog3",
+  storageBucket: "ti-prog3.firebasestorage.app",
+  messagingSenderId: "24323296903",
+  appId: "1:24323296903:web:90c57566bb3c630b3af222"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
