1e9fc2096c582cc0927502f9fe5c6ac5a9f18880
Oli Roldan <oroldan@udesa.edu.ar>
2024-11-14 20:50:06 -0300

    punto 2 navigation-empezado

---
+++ src/screens/Home.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Home</Text>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('Login')}>
+                    <Text>Ir a Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.props.navigation.navigate('Register')}>
+                    <Text>Ir a Register</Text>
+                </TouchableOpacity>
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
---
+++ src/screens/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native-web'
+
+export default class Login extends Component {
+  render() {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    )
+  }
+}
