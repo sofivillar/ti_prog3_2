5461fa7f82907528a53c0e345ff650c3e2f6bbf5
Oli Roldan <oroldan@udesa.edu.ar>
2024-11-18 12:33:39 -0300

    tab navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Register from './src/screens/Register';
+import HomeMenu from './src/components/HomeMenu';
 import Home from './src/screens/Home';
 
 const Stack =createNativeStackNavigator();
@@ -11,14 +12,12 @@ const Stack =createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={Home}  options={{headerShown: false}}/>
-      <Stack.Screen name='Login' component={Login}/> 
-      <Stack.Screen name='Register' component={Register}/>
-
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name='HomeMenu' component={HomeMenu} options={{ headerShown: false }} />
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Register' component={Register} />
+      </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
 
---
+++ src/components/HomeMenu.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Home from "../screens/Home";
+import NewPost from '../screens/NewPost';
+import Profile from '../screens/Profile';
+import Users from '../screens/Users';
+
+const Tab = createBottomTabNavigator();
+
+class HomeMenu extends Component {
+
+    render() {
+        return ( 
+            <Tab.Navigator screenOptions={{ tabBarShowLabel: false, headerShown: false }}>
+                <Tab.Screen name="Home" component={Home} options={{ tabBarIcon: () => <AntDesign name="home" size={24} color="black" /> }} />
+                <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: () => <AntDesign name="user" size={24} color="black" /> }} />
+                <Tab.Screen name="Users" component={Users} options={{ tabBarIcon: () => <AntDesign name="team" size={24} color="black" /> }} />
+                <Tab.Screen name="NewPost" component={NewPost} options={{ tabBarIcon: () => <AntDesign name="picture" size={24} color="black" /> }} />
+            </Tab.Navigator>
+        );
+    }
+}
+
+export default HomeMenu;
