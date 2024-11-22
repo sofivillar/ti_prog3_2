a3d941acd3906dbe53f51fa376e6bb6110c74bc6
Alejandro Villar <villars@udesa.edu.ar>
2024-11-22 00:06:48 -0300

    Crear posteo wip

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { auth } from '../firebase/config';
 
 export class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: this.props.description,
+      email: ''
+    }
+  }
+  componentDidMount() {
+    {if (auth.currentUser === null) 
+      this.props.navigation.navigate("HomeMenu")
+     }
+    /* auth.onAuthStateChanged(user => {
+      if (user) {
+        this.props.navigation.navigate('HomeMenu')
+// SI HAY ALGIEN LOGUEADO, que pueda crear posteo 
+      }
+    }
+    ) */
+  }
   render() {
     return (
-      <div>
-        New Post
-      </div>
+      <View>
+        <Text>Create New Post</Text>
+        <TextInput
+          keyboardType='default'
+          placeholder='Describe what you are thinking...'
+          onChange={text => this.setState({ description: text })}
+          value={this.state.description} />
+        <TouchableOpacity onPress={() => this.handleSubmit()}></TouchableOpacity>
+      </View>
     )
   }
 }
