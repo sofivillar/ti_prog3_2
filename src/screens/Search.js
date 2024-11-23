21b2b32284b26de6653d93d4df2113e5ff473cef
Alejandro Villar <villars@udesa.edu.ar>
2024-11-23 15:28:37 -0300

    Buscador

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, TextInput } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, FlatList } from 'react-native'
+import { auth, db } from "../firebase/config";
 
 export class Search extends Component {
 
-  /* handleSearch(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      busqueda: ""
+    }
+  }
 
-  } */
+  componentDidMount() {
+    db.collection("users")
+      .onSnapshot(docs => {
+        let users = [];
+
+
+        docs.forEach(doc => {
+          users.push({ id: doc.id, data: doc.data() })
+        });
+        console.log("Sanpshot");
+
+      })
+  }
+
+  handleSearch() {
+    return this.state.users.filter((usuario) => { return usuario.data.username.toLowerCase().includes(this.state.busqueda.toLowerCase()) })
+  }
 
   render() {
+    console.log(this.state.users);
+
+    const resultadosBusqueda = this.handleSearch()
+
     return (
-      <Text>Buscador de usuarios</Text>
+      <View>
+        <Text>Buscador de usuarios</Text>
+        <TextInput placeholder='Buscador' keyboardType='default' value={this.state.busqueda} onChangeText={(texto) => this.setState({ busqueda: texto })} />
+        {resultadosBusqueda.length === 0 ? (<Text>No hay resultados</Text>) : (<FlatList data={this.state.posteos} keyExtractor={(item) => item.id.toString()} renderItem={({ item }) => (item.data.username)} />)}
+      </View>
     )
   }
 }
